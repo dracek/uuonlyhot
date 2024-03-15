ed7a8e32ce41b5973a3758413d234c4176a8010f
Ren <buher1994@gmail.com>
2024-03-15 17:25:29 +0100

    add test a temp app/api/controllers/onlyhot-main-controller.js

--- uu_onlyhot_maing01-server/app/api/controllers/onlyhot-main-controller.js
+++ uu_onlyhot_maing01-server/app/api/controllers/onlyhot-main-controller.js
@@ -1,7 +1,14 @@
 "use strict";
 const OnlyhotMainAbl = require("../../abl/onlyhot-main-abl.js");
-
+const { temp } = require("../../abl/temp-abl.js")
 class OnlyhotMainController {
+
+  test(ucEnv) {
+    return OnlyhotMainAbl.test(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
+  }
+  temp(ucEnv) {
+    return temp(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
+  }
   init(ucEnv) {
     return OnlyhotMainAbl.init(ucEnv.getUri(), ucEnv.getDtoIn(), ucEnv.getSession());
   }
