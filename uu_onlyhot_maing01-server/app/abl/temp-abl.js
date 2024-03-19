2c2a5e09fd42a0c82d3ef50af64544a02b20bbd4
Ren <buher1994@gmail.com>
2024-03-19 20:33:15 +0100

    Přidání komentáře k nedokončeným změnám

--- uu_onlyhot_maing01-server/app/abl/temp-abl.js
+++ uu_onlyhot_maing01-server/app/abl/temp-abl.js
@@ -8,43 +8,27 @@ const { LoggerFactory } = require("uu_appg01_server").Logging;
 const { AppClient } = require("uu_appg01_server");
 const Errors = require("../api/errors/onlyhot-main-error.js");
 const OnlyhotMainMongo = require("../dao/temp-mongo.js");
+const { DaoFactory,ObjectStoreError } = require("uu_appg01_server").ObjectStore;
 
 const logger = LoggerFactory.get("OnlyhotMainAbl");
 const validator = Validator.load();
 
 async function temp(awid, dtoIn) {
-  try {
-    // Validace dtoIn
-    //let validationResult = validator.validate("tempDtoInType", dtoIn);
-    //ValidationHelper.processValidationResult(
-    //  dtoIn,
-    //  validationResult,
-    //  Errors.Temp.InvalidDtoIn
-   // );
 
-    const { data } = dtoIn;
+  for (const item of dtoIn.data) {
+    // Vytvoření objektu s teplotními daty pro aktuální položku
     const temperatureData = {
-      awid: awid,
-      temperatures: data.map(item => parseFloat(item.temperature)),
-      timestamp: new Date()
+        awid: awid,
+        temperature: parseFloat(item.temperature),
+        timestamp: item.timestamp
     };
 
-    // Vytvoření instance OnlyhotMainMongo
-    const onlyhotMainMongo = new OnlyhotMainMongo();
+    const dao = DaoFactory.getDao("temp");
 
-    // Volání metody create na objektu OnlyhotMainMongo
-    await onlyhotMainMongo.create(awid, temperatureData);
-
-    const itemCount = data.length;
-
-    const dtoOut = {
-      message: `Dostala jsem pole, počet položek ${itemCount}, teploty: ${temperatureData.temperatures.join(", ")}`
-    };
-    return dtoOut;
-  } catch (error) {
-    logger.error("Error occurred while processing the request:", error);
-    return { error: "An error occurred while processing the request." };
+    await dao.create(temperatureData);
   }
+    return " ... huraaa ";
+
 }
 
 module.exports = {
