ba27d9b402c61ccf939ba7175fd8354e0838e41e
Ren <buher1994@gmail.com>
2024-03-25 19:03:49 +0100

    update

--- uu_onlyhot_maing01-server/app/abl/temp-abl.js
+++ uu_onlyhot_maing01-server/app/abl/temp-abl.js
@@ -7,8 +7,7 @@ const { AppClient } = require("uu_appg01_server");
 const Errors = require("../api/errors/onlyhot-main-error.js");
 const { DaoFactory, ObjectStoreError } = require("uu_appg01_server").ObjectStore;
 
-const logger = LoggerFactory.get("OnlyhotMainAbl");
-const validator = Validator.load();
+
 const WARNINGS = {
   Temp: {
     UnsupportedKeys: {
@@ -17,42 +16,47 @@ const WARNINGS = {
   },
 };
 
-async function temp(awid, dtoIn) {
-  const dao = DaoFactory.getDao("temp");
-
-  for (const item of dtoIn.data) {
-    let validationResult = validator.validate("tempDtoInType", item);
-    let uuAppErrorMap = ValidationHelper.processValidationResult(
-      item,
-      validationResult,
-      {},
-      WARNINGS.Temp.UnsupportedKeys.code,
-      Errors.Create.InvalidDtoIn
-    );
-
-    // Přidělení ownerId do temperatureData pro každou položku
-    const temperatureData = {
-      awid: awid,
-      temperature: parseFloat(item.temperature),
-      timestamp: item.timestamp,
-      ownerId: session.getIdentity().getUuIdentity() // Přidělení ownerId
-    };
-
-    let dtoOut;
-    try {
-      dtoOut = await dao.temp(temperatureData);
-    } catch (e) {
-      if (e instanceof ObjectStoreError) {
-        throw new Errors.Create.TempDaoFailed({ uuAppErrorMap }, e);
-      }
-      throw e;
-    }
-    dtoOut.uuAppErrorMap = uuAppErrorMap;
-    return dtoOut;
+class Temp {
+  constructor() {
+    this.validator = Validator.load();
+    this.dao = DaoFactory.getDao("temp");
   }
-  return " ... huraaa ";
+
+  async temp(awid, dtoIn) {
+    const dao = DaoFactory.getDao("temp");
+    let result = [];
+
+    for (const item of dtoIn.data) {
+      let validationResult = this.validator.validate("tempDtoInType", item);
+      let uuAppErrorMap = ValidationHelper.processValidationResult(
+        item,
+        validationResult,
+        {},
+        WARNINGS.Temp.UnsupportedKeys.code,
+        Errors.Create.InvalidDtoIn
+      );
+
+  
+      const temperatureData = {
+        awid: awid,
+        temperature: parseFloat(item.temperature),
+        timestamp: item.timestamp,
+      };
+
+      let dtoOut;
+      try {
+        dtoOut = await dao.temp(temperatureData);
+        dtoOut.uuAppErrorMap = uuAppErrorMap;
+        result.push(dtoOut);
+      } catch (e) {
+        if (e instanceof ObjectStoreError) {
+          throw new Errors.Create.TempDaoFailed({ uuAppErrorMap }, e);
+        }
+        throw e;
+      }
+    }
+    return result;
+  }
 }
 
-module.exports = {
-  temp
-};
+module.exports = Temp;
