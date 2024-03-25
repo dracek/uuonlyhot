40d1a71c06e170a47856776452f2777c2309a13a
Ren <buher1994@gmail.com>
2024-03-25 19:46:46 +0100

    update

--- uu_onlyhot_maing01-server/app/api/controllers/onlyhot-main-controller.js
+++ uu_onlyhot_maing01-server/app/api/controllers/onlyhot-main-controller.js
@@ -1,13 +1,13 @@
 "use strict";
 const OnlyhotMainAbl = require("../../abl/onlyhot-main-abl.js");
-const { temp } = require("../../abl/temp-abl.js")
+const Temp = require("../../abl/temp-abl.js")
 class OnlyhotMainController {
 
   test(ucEnv) {
     return OnlyhotMainAbl.test(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
   }
   temp(ucEnv) {
-    return temp(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
+    return Temp.temp(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
   }
   init(ucEnv) {
     return OnlyhotMainAbl.init(ucEnv.getUri(), ucEnv.getDtoIn(), ucEnv.getSession());
